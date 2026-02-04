7f5cfea026e5cd7a286d49f30e127d6af487f8ed
tseringwangdue328-beep <tseringwangdue328@gmail.com>
2026-02-04 10:35:22 +0545

    my first javascript

---
+++ script.js
@@ -0,0 +1,52 @@
+// console.log("unwanted buggy code")
+console.log("Hello world")
+/* et color = "red"
+console.log("color", color) */
+/* let fullname = "Shubangand"
+console.log("My name is", fullname)
+
+fullname = "Ram Kumar"
+console.log(fullname);
+fullname = "Hari Kumar"
+console.log(fullname);
+fullname = "Hari Bahadur"
+console.log(fullname); */
+let firstName = "Hari"
+let middleName = "Kumar"
+middleName = "Bahadur"
+lastName = "oli"
+let fullName = firstName + " " + middleName + " "+ lastName
+console.log(fullName)
+
+const PI = 3.1415926533589793;
+// const PI = 3.14;
+console.log(PI.toFixed(2));
+let a = 1;
+let b =2;
+let c;
+console.log(a,b);
+c = a;
+a = b;
+b =c;
+console.log(a,b);
+console.log("double quote = ",'""')
+let color ={
+    name: "red",
+    hexValue: "#FF0000"
+}
+console.log(`hex value of color ${color.name} is ${color.hexValue}`)
+console.log(color)
+let myLaptop ={
+    name: "LOQ",
+    stroage: "Ram 16gp"
+} 
+console.log(myLaptop)
+let person ={
+    first_name: "john",
+    last_name: "doe",
+    age: "22",
+    website: "john@doe.com"
+
+    
+}
+console.log(`full name is ${person.first_name} ${person.last_name}`);
